1526fe8efe143ef5dde97eeb4bac52dfea599398
GeorgeCottrell <georgecottrell@email.com>
2023-08-14 21:03:16 -0600

    Update SpaceShowdown_V13.py

--- SpaceShowdown_V13.py
+++ SpaceShowdown_V13.py
@@ -4,37 +4,54 @@ import random
 import os
 import pickle
 
+# Initialize pygame
 pygame.init()
 
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Space Showdown")
+# Set display mode to be resizable
+screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
 
+# Get the current display info
+screen_info = pygame.display.Info()
+
+# Calculate the dimensions for the windowed full screen
+target_width = screen_info.current_w
+target_height = screen_info.current_h
+aspect_ratio = target_width / target_height
+
+# Set initial screen dimensions to be the calculated size
+screen_width = target_width
+screen_height = target_height
+
+# Load images
 background_image = pygame.image.load("background.jpg")
 ship_image = pygame.image.load("ship.png")
 enemy_image = pygame.image.load("enemy.png")
+projectile_image = pygame.image.load("projectile.png")
 
+# Define colors
 black = (0, 0, 0)
 white = (255, 255, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
 orange = (255, 165, 0)
 
+# Define fonts
 font = pygame.font.Font(None, 36)
 title_font = pygame.font.Font(None, 72)
 
+# Initialize clock
 clock = pygame.time.Clock()
 
+# Define game states
 START_SCREEN = 0
 GAME_RUNNING = 1
 GAME_OVER = 2
 LEADERBOARD = 3
 game_state = START_SCREEN
 
+# Load high scores from file if it exists
 scores_file = "high_scores.pkl"
 high_scores = {}
-
 if os.path.exists(scores_file):
     with open(scores_file, "rb") as f:
         high_scores = pickle.load(f)
@@ -49,7 +66,8 @@ def generate_enemies(round_num):
         enemy_speed = random.uniform(1, 3) + (round_num * 0.2)
         enemy_x = random.randint(0, screen_width - enemy_width)
         enemy_y = random.randint(-600, -enemy_height)
-        new_enemies.append([enemy_x, enemy_y, enemy_speed])
+        enemy_movement_speed = random.uniform(0.5, 1.5)
+        new_enemies.append([enemy_x, enemy_y, enemy_speed, enemy_movement_speed])
     return new_enemies
 
 def display_text(text, x, y, color=white, font=None):
@@ -58,17 +76,24 @@ def display_text(text, x, y, color=white, font=None):
     text_surface = font.render(text, True, color)
     screen.blit(text_surface, (x, y))
 
+# Player ship dimensions and initial position
 player_width = 50
 player_height = 50
 player_x = (screen_width - player_width) // 2
 player_y = screen_height - player_height - 10
 player_speed = 5
 
+# Number of enemies and their dimensions
 num_enemies = 25
 enemy_width = 50
 enemy_height = 50
-enemies = generate_enemies(1)
 
+# Initialize enemy projectiles
+enemy_projectiles = []
+enemy_projectile_speed = 7
+
+# Initialize other game variables
+enemies = generate_enemies(1)
 running = True
 score = 0
 streak = 0
@@ -85,11 +110,26 @@ projectile = None
 projectile_fired = False
 initials_text = ""
 
+# Initialize variables for enemy movement patterns
+movement_direction = 1  # 1 for right, -1 for left
+movement_amplitude = 20
+
+# New variables for spinning animation and ship visibility
+player_spinning = False
+player_visible = True
+spin_angle = 0
+spin_direction = 1
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        elif event.type == pygame.VIDEORESIZE:
+            # Update screen dimensions when window is resized
+            screen_width = event.w
+            screen_height = event.h
+            screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+    
     keys = pygame.key.get_pressed()
 
     if game_state == START_SCREEN:
@@ -132,6 +172,8 @@ while running:
         for enemy in enemies:
             enemy_speed = enemy[2]
             enemy[1] += enemy_speed
+            enemy_movement_speed = enemy[3]
+            enemy[0] += movement_direction * enemy_movement_speed
             
             if projectile:
                 enemy_rect = pygame.Rect(enemy[0], enemy[1], enemy_width, enemy_height)
@@ -149,6 +191,8 @@ while running:
             if enemy[1] >= screen_height:
                 enemy[1] = -enemy_height
                 enemy[0] = random.randint(0, screen_width - enemy_width)
+                enemy_movement_speed = random.uniform(0.5, 1.5)
+                enemy[3] = enemy_movement_speed
 
             if not player_invincible:
                 player_rect = pygame.Rect(player_x, player_y, player_width, player_height)
@@ -179,6 +223,12 @@ while running:
             current_round += 1
             enemies = generate_enemies(current_round)
 
+            # Update enemy movement pattern
+            if current_round % 2 == 0:
+                movement_direction *= -1
+            for enemy in enemies:
+                enemy[0] += movement_direction * movement_amplitude
+
         screen.fill(black)
         screen.blit(background_image, (0, 0))
         screen.blit(ship_image, (player_x, player_y))
@@ -194,6 +244,38 @@ while running:
         display_text("Round Bonus: " + str(round_bonus), 10, 90)
         display_text("Round: " + str(current_round), 10, 130)
         display_text("Lives: " + str(lives), screen_width - 150, 10)
+
+
+        # Check for enemy projectiles hitting the player
+        for proj in enemy_projectiles:
+            proj[1] += enemy_projectile_speed
+            if not player_invincible and not player_spinning:
+                player_rect = pygame.Rect(player_x, player_y, player_width, player_height)
+                projectile_rect = pygame.Rect(proj[0], proj[1], 1, 1)
+                if player_rect.colliderect(projectile_rect):
+                    lives -= 1
+                    if lives > 0:
+                        player_visible = False
+                        player_spinning = True
+                        spin_angle = 0
+                        spin_direction = 1
+                        player_invincible = True
+                        invincible_frames = invincible_duration
+                        streak = 0
+                    else:
+                        game_state = GAME_OVER
+            if proj[1] > screen_height:
+                enemy_projectiles.remove(proj)
+
+        # Enemy ship shooting logic for round 3 onward
+        if current_round >= 3:
+            for enemy in enemies:
+                if random.random() < 0.01:
+                    enemy_proj_x = enemy[0] + enemy_width // 2
+                    enemy_proj_y = enemy[1] + enemy_height
+                    enemy_projectiles.append([enemy_proj_x, enemy_proj_y])
+
+        # ... (Other game logic)
 
     elif game_state == GAME_OVER:
         screen.fill(black)
